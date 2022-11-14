d8d7a1e853c45b7aafc9d3a378e7fc682fe652b0
NekoYo1 <kostyafear203@gmail.com>
2022-11-14 11:52:46 +1000

    Добавьте файлы проекта.

---
+++ PR1_TEST_FILIPPOV/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1_TEST_FILIPPOV
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
